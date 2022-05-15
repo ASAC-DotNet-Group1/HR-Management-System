35b6ffe76e9d5ef3000554153a156a08d81e5b90
LaithAlamat <laithalamat4@gmail.com>
2022-05-15 14:35:12 +0300

    add performance method completed

--- HR-Management-System/HR-Management-System/Models/DTOs/PerformanceDTO.cs
+++ HR-Management-System/HR-Management-System/Models/DTOs/PerformanceDTO.cs
@@ -8,12 +8,12 @@ namespace HR_Management_System.Models.DTOs
     public class PerformanceDTO
     {
         public int ID { get; set; }
-        public int Attendance { get; set; }
-        public int Effeciency { get; set; }
+        public int Commitment { get; set; }
+        public int Efficiency { get; set; }
         public int Communication { get; set; }
         public int TimeManagement { get; set; }
         public int QualityOfWork { get; set; }
-        public int Overall { get; set; }
+        public double Overall { get; set; }
         public DateTime PerformanceDate { get; set; }
         public EmployeeDTO Employee { get; set; }
     }
--- HR-Management-System/HR-Management-System/Models/Performance.cs
+++ HR-Management-System/HR-Management-System/Models/Performance.cs
@@ -9,14 +9,14 @@ namespace HR_Management_System.Models
     {
         public int ID { get; set; }
         public int EmployeeID { get; set; }
-        public int Attendance { get; set; }
-        public int Effeciency { get; set; }
+        public int Commitment { get; set; }
+        public int Efficiency { get; set; }
         public int Communication { get; set; }
         public int TimeManagement { get; set; }
         public int QualityOfWork { get; set; }
-        public int Overall { get; set; }
+        public double Overall { get; set; }
         public DateTime PerformanceDate { get; set; }
         public Employee Employee { get; set; } 
     }
-}
+} 
  
